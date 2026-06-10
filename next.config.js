aaf0da49cee42c9f0ee7418c55cd9185059d2a3f
Abdul Qoyyuum <aqoyyuum@cardaccess.com.au>
2026-06-10 09:55:25 +0800

    :rocket: Used the modern next build to handle asset exports

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 
 module.exports = ({
   output: "export",
+  distDir: 'dist',
   pageExtensions: ["tsx"],
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push(
